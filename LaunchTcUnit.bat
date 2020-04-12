a8bec03d2074b7d96c08329214101cd6b8d612c4
Jakob Sagatowski <jakob.sagatowski@outlook.com>
2020-04-12 22:31:34 +0200

    Added parameters for TCUNIT_TASK_NAME and TCUNIT_AMSNETID for Windows batch script file.

--- LaunchTcUnit.bat
+++ LaunchTcUnit.bat
@@ -1,39 +1,34 @@
 @echo off
-
-rem The MIT License(MIT)
-
-rem Copyright(c) 2020 Jakob Sagatowski
-
-rem Permission is hereby granted, free of charge, to any person obtaining a copy of
-rem this software and associated documentation files (the "Software"), to deal in
-rem the Software without restriction, including without limitation the rights to
-rem use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-rem the Software, and to permit persons to whom the Software is furnished to do so,
-rem subject to the following conditions:
-
-rem The above copyright notice and this permission notice shall be included in all
-rem copies or substantial portions of the Software.
-
-rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-rem FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-rem COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-rem IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-rem CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 rem -----------------------------------------------------------------------------
-rem USAGE:
+rem Batch script for launching TcUnit-Runner
+rem For documentation see: www.tcunit.org
+rem -----------------------------------------------------------------------------
+rem Instructions:
 rem Set the below variable TCUNIT_RUNNER_INSTALL_DIRECTORY to where TcUnit-Runner was
 rem installed. This should normally be left at default and not changed
 rem (default C:\Program Files (x86)\TcUnit-Runner)
 SET TCUNIT_RUNNER_INSTALL_DIRECTORY=C:\Program Files (x86)\TcUnit-Runner
 
-rem The parameters that need to be supplied are
-rem %1 - The name of unit test task
-SET TCUNIT_TASK_NAME=%1
+rem The parameters that can be supplied are:
+rem -t [OPTIONAL] The name of the task running TcUnit defined under "Tasks".
+rem    If this is not provided, it's assumed that only one task exists in the TwinCAT project.
+rem -a [OPTIONAL] The AMS NetId of the device of where the project and TcUnit should run.
+rem    If this is not provided, the local AMS NetId is assumed (127.0.0.1.1.1)
+SET TCUNIT_TASK_NAME=
+SET TCUNIT_AMSNETID=
 
+CALL :Process_Parameters %1, %2, %3, %4
 
-set TCUNIT_RUNNER_EXECUTABLE_COMPLETE_PATH=%TCUNIT_RUNNER_INSTALL_DIRECTORY%\TcUnit-Runner.exe
+rem Create parameter call to TcUnit-Runner
+SET TCUNIT_RUNNER_PARAMETERS=
+IF DEFINED TCUNIT_TASK_NAME (
+    SET TCUNIT_RUNNER_PARAMETERS=%TCUNIT_RUNNER_PARAMETERS% --TcUnitTaskName=%TCUNIT_TASK_NAME%
+)
+IF DEFINED TCUNIT_AMSNETID (
+    SET TCUNIT_RUNNER_PARAMETERS=%TCUNIT_RUNNER_PARAMETERS% --AmsNetId=%TCUNIT_AMSNETID%
+)
+
+SET TCUNIT_RUNNER_EXECUTABLE_COMPLETE_PATH=%TCUNIT_RUNNER_INSTALL_DIRECTORY%\TcUnit-Runner.exe
 
 rem Check that the TcUnit-Runner executable exists
 IF NOT EXIST "%TCUNIT_RUNNER_EXECUTABLE_COMPLETE_PATH%" (
@@ -41,15 +36,17 @@ IF NOT EXIST "%TCUNIT_RUNNER_EXECUTABLE_COMPLETE_PATH%" (
     GOTO Exit
 )
 
-rem Error handling of providing TcUnit task name
 IF NOT DEFINED TCUNIT_TASK_NAME (
     echo Task name of the TcUnit task not provided! Assuming only one task in TwinCAT solution
 ) ELSE (
     echo A TcUnit task name has been provided, using: %TCUNIT_TASK_NAME%
 )
 
-rem Enter Jenkins workspace (stored in environment variable %WORKSPACE%)
-cd %WORKSPACE%
+IF NOT DEFINED TCUNIT_AMSNETID (
+    echo AmsNetId to run TwinCAT/TcUnit is not provided! Assuming TwinCAT/TcUnit will run locally '127.0.0.1.1.1'
+) ELSE (
+    echo An AMS NetId has been provided, using: %TCUNIT_AMSNETID%
+)
 
 rem Find the visual studio solution file.
 FOR /r %%i IN (*.sln) DO (
@@ -65,18 +62,46 @@ IF NOT DEFINED VISUAL_STUDIO_SOLUTION_PATH (
     echo The filepath to the visual studio solution file is: %VISUAL_STUDIO_SOLUTION_PATH%
 )
 
-
 rem Call TcUnit-Runner
-IF NOT DEFINED TCUNIT_TASK_NAME (
-    "%TCUNIT_RUNNER_EXECUTABLE_COMPLETE_PATH%" --VisualStudioSolutionFilePath=%VISUAL_STUDIO_SOLUTION_PATH%
-) ELSE (
-    "%TCUNIT_RUNNER_EXECUTABLE_COMPLETE_PATH%" --VisualStudioSolutionFilePath=%VISUAL_STUDIO_SOLUTION_PATH% --TcUnitTaskName=%TCUNIT_TASK_NAME%
-)
+"%TCUNIT_RUNNER_EXECUTABLE_COMPLETE_PATH%" --VisualStudioSolutionFilePath=%VISUAL_STUDIO_SOLUTION_PATH% %TCUNIT_RUNNER_PARAMETERS%
 
 rem %errorlevel% is a system wide environment variable that is set upon execution of a program
 echo Exit code is %errorlevel%
 
 EXIT /B %errorlevel%
+
+rem This function process the parameters 
+:Process_Parameters
+
+rem First parameter
+IF "%~1" == "-t" (
+    SET TCUNIT_TASK_NAME=%2
+)
+IF "%~1" == "-T" (
+    SET TCUNIT_TASK_NAME=%2
+)
+IF "%~1" == "-a" (
+    SET TCUNIT_AMSNETID=%2
+)
+IF "%~1" == "-A" (
+    SET TCUNIT_AMSNETID=%2
+)
+
+rem Second parameter
+IF "%~3" == "-t" (
+    SET TCUNIT_TASK_NAME=%4
+)
+IF "%~3" == "-T" (
+    SET TCUNIT_TASK_NAME=%4
+)
+IF "%~3" == "-a" (
+    SET TCUNIT_AMSNETID=%4
+)
+IF "%~3" == "-A" (
+    SET TCUNIT_AMSNETID=%4
+)
+
+GOTO:EOF
 
 :Exit
 echo Failed!
