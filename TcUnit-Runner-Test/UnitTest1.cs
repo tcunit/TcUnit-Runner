94d5941795cb299461432bcee993786a2dc9a8ca
Jakob Sagatowski <jakob.sagatowski@outlook.com>
2020-08-10 09:45:58 +0200

    Added shell for unit tests.

---
+++ TcUnit-Runner-Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TcUnit_Runner_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
