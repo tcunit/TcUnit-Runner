84590cd3a61e48089c46935f3f44218e80f3dd9d
densogiaichned <66565474+densogiaichned@users.noreply.github.com>
2023-11-11 18:55:28 +0100

    fix: Always find *.tsproj in sln-file

--- TcUnit-Runner/TcFileUtilities.cs
+++ TcUnit-Runner/TcFileUtilities.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TcUnit.TcUnit_Runner
 {
-    class TcFileUtilities
+    public class TcFileUtilities
     {
         private static ILog log = LogManager.GetLogger("TcUnit-Runner");
 
@@ -59,30 +60,24 @@ namespace TcUnit.TcUnit_Runner
             string tcProjectFilePath = "";
             string tcProjectFile = "";
 
-            System.IO.StreamReader file = new System.IO.StreamReader(@VisualStudioSolutionFilePath);
-            while ((line = file.ReadLine()) != null)
+            using (System.IO.StreamReader file = new System.IO.StreamReader(@VisualStudioSolutionFilePath))
             {
-                if (line.StartsWith("Project"))
+
+                while ((line = file.ReadLine()) != null)
                 {
-                    tcProjectFile = Utilities.GetUntilOrEmpty(line, ".tsproj");
-                    break;
+                    var match = Regex.Match(line, "Project\\(\"{B1E792BE-AA5F-4E3C-8C82-674BF9C0715B}\"\\) = \"(.+)\", \"(?<tsproj>.+\\.tsproj)\"", RegexOptions.IgnoreCase | RegexOptions.Singleline, TimeSpan.FromSeconds(5));
+                    if (match.Success)
+                    {
+                        tcProjectFile = match.Groups["tsproj"].Value;
+                        break;
+                    }
                 }
+                file.Close();
             }
-            file.Close();
 
             if (!String.IsNullOrEmpty(tcProjectFile))
             {
-                int indexOfTcProjectFile = tcProjectFile.LastIndexOf("\"") + 1;
-                try { 
-                    tcProjectFile = tcProjectFile.Substring(indexOfTcProjectFile, (tcProjectFile.Length- indexOfTcProjectFile));
-
-                    // Add visual studio solution directory path
-                    string VisualStudioSolutionDirectoryPath = Path.GetDirectoryName(VisualStudioSolutionFilePath);
-                    tcProjectFilePath = VisualStudioSolutionDirectoryPath + "\\" + tcProjectFile + ".tsproj";
-                } catch
-                {
-
-                }
+                tcProjectFilePath = Path.Combine(Path.GetDirectoryName(VisualStudioSolutionFilePath), tcProjectFile);
             }
             return tcProjectFilePath;
         }
