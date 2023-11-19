5bdd2287e84344eaa9ab09cf3a14217d01784b5d
densogiaichned <66565474+densogiaichned@users.noreply.github.com>
2023-11-19 18:02:50 +0100

    feat: add plugin VariantManager
    This plugin is executed on hook 'after soltuion loaded'.
    It uses the latest ITcSysManager15 interface.

---
+++ plugins/VariantManager-Plugin/AutomationInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TCatSysManagerLib;
+
+namespace TcUnit.TcUnit_Runner.Plugin
+{
+    internal class AutomationInterface
+    {
+        private ITcSysManager15 sysManager = null;
+
+        public AutomationInterface(EnvDTE.Project project)
+        {
+            sysManager = (ITcSysManager15)project.Object;
+        }
+
+        public ITcSysManager15 ITcSysManager => this.sysManager;
+    }
+}
---
+++ plugins/VariantManager-Plugin/VariantManagerPlugin.cs
@@ -0,0 +1,64 @@
+﻿using EnvDTE;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TcUnit.TcUnit_Runner.PluginManager.Options;
+using TcUnit.TcUnit_Runner.PluginManager.Plugins;
+
+namespace TcUnit.TcUnit_Runner.Plugin
+{
+
+    [Export(typeof(IAfterSolutionLoadedHook))]
+    public class VariantManagerPlugin : IAfterSolutionLoadedHook
+    {
+        private ILog _log;
+
+        public string Name { get { return "VariantManager"; } }
+
+        public int Run(Project project, ILog log, PluginOptions options)
+        {
+            _log = log;
+            AutomationInterface ai = new AutomationInterface(project);
+            return SetProjectVariant(ai, options);
+        }
+
+
+        private int SetProjectVariant(AutomationInterface automationInterface, PluginOptions options)
+        {
+            var variant = GetVariantFromOptions(options);
+
+            if (string.IsNullOrEmpty(variant))
+            {
+                _log.Error("No variant provided");
+                return -1;
+            }
+
+            try
+            {
+                //Using newer version of ITcSysManager to be able to set variant
+                automationInterface.ITcSysManager.CurrentProjectVariant = variant;
+                _log.Info("Variant selected with name: " + variant);
+                // Wait
+                System.Threading.Thread.Sleep(1000);
+                return 0;
+            }
+            catch
+            {
+                _log.Error("Unable to set variant: " + variant + ". Please provide an existing variant from the project.");
+                return -1;
+            }
+
+        }
+
+        private string GetVariantFromOptions(PluginOptions options)
+        {
+            var plugin = options.GetPluginNodeByAttributeName(Name);
+            return plugin.GetPluginNodeValueByName("Variant");
+        }
+
+    }
+}
