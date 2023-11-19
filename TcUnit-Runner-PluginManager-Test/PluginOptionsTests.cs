138b5fea52ba5d5eb2c84e9f4637c6be5d924721
densogiaichned <66565474+densogiaichned@users.noreply.github.com>
2023-11-19 18:01:05 +0100

    tests: Add test for plugin manager

---
+++ TcUnit-Runner-PluginManager-Test/PluginOptionsTests.cs
@@ -0,0 +1,52 @@
+﻿using log4net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml;
+using TcUnit.TcUnit_Runner.PluginManager;
+using TcUnit.TcUnit_Runner.PluginManager.Options;
+
+namespace TcUnit_Runner_PluginManager_Test
+{
+    [TestClass]
+    public class PluginOptionsTests
+    {
+
+        private static ILog log = LogManager.GetLogger("TcUnit-Runner-PluginManager-Test");
+        [TestMethod]
+        public void ShouldSuccessfullyLoadOptionsFromFile()
+        {
+            PluginLoader sut = new PluginLoader(log);
+            sut.LoadOptions("test-files/.TcUnitRunner");
+            Assert.AreEqual(sut.Options.Options.ChildNodes.Count, 2);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNodeWithNameTestPlugin()
+        {
+            PluginLoader sut = new PluginLoader(log);
+            sut.LoadOptions("test-files/.TcUnitRunner");
+            var actual = sut.Options.GetPluginNodeByAttributeName("TestPlugin");
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNodeValueWithNameTestNode()
+        {
+            PluginLoader sut = new PluginLoader(log);
+            sut.LoadOptions("test-files/.TcUnitRunner");
+            var node = sut.Options.GetPluginNodeByAttributeName("TestPlugin");
+            var actual = node.GetPluginNodeValueByName("TestNode");
+            Assert.IsNotNull(actual);
+            Assert.AreEqual("test-value", actual);
+        }
+
+        [TestMethod]
+        public void ShouldReturnAttributeValuePluginsDir()
+        {
+            PluginLoader sut = new PluginLoader(log);
+            sut.LoadOptions("test-files/.TcUnitRunner");
+            var actual = sut.Options.GetPluginsDirectory();
+            Assert.IsNotNull(actual);
+            Assert.AreEqual("./plugins", actual);
+        }
+    }
+}
---
+++ TcUnit-Runner-PluginManager-Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TcUnit-Runner-PluginManager-T")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TcUnit-Runner-PluginManager-T")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e985d732-a453-4d3e-a99f-52d368203ae8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
